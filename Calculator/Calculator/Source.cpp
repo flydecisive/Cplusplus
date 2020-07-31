467744705d3689183151458612478960e19dc8a0
flydecisive <45427901+flydecisive@users.noreply.github.com>
2020-07-31 13:07:13 +0400

    Update Source.cpp
    Изменил вывод результата и список команд

--- Calculator/Calculator/Source.cpp
+++ Calculator/Calculator/Source.cpp
@@ -14,29 +14,31 @@ int main() {
 	cin >> fNum;
 	cout << "Введите второе число: ";
 	cin >> sNum;
-	cout << endl;
-	cout << "Список команд: " << "\n"
-		<< "Сложение: +" << "\n"
-		<< "Вычитание: -" << "\n"
-		<< "Умножение: *" << "\n"
-		<< "Целочисленное деление: /" << "\n"
-		<< "Взятие остатка: %" << "\n";
+	cout << endl << endl;
+	cout << "\t" << "###Список команд:###" << "\n"
+		<< "\t" << "Сложение: +" << "\n"
+		<< "\t" << "Вычитание: -" << "\n"
+		<< "\t" << "Умножение: *" << "\n"
+		<< "\t" << "Целочисленное деление: /" << "\n"
+		<< "\t" << "Взятие остатка: %" << "\n" << endl;
+	cout << "Введите команду: ";
 	cin >> operation;
+	cout << fNum << " " << operation << " " << sNum << " = ";
 	switch (operation) {
 	case '+':
-		cout << fNum << " + " << sNum << " = " << fNum + sNum;
+		cout << fNum + sNum;
 		break;
 	case '-':
-		cout << fNum << " - " << sNum << " = " << fNum - sNum;
+		cout << fNum - sNum;
 		break;
 	case '*':
-		cout << fNum << " * " << sNum << " = " << fNum * sNum;
+		cout << fNum * sNum;
 		break;
 	case '/':
-		cout << fNum << " / " << sNum << " = " << fNum / sNum;
+		cout << fNum / sNum;
 		break;
 	case '%':
-		cout << fNum << " % " << sNum << " = " << fNum % sNum;
+		cout << fNum % sNum;
 		break;
 	default:
 		cout << "Я еще не научился выполнять такие операции:(";
