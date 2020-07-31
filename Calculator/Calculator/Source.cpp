73d4af53611b3a711629379f36ea25b093329489
flydecisive <45427901+flydecisive@users.noreply.github.com>
2020-07-31 13:20:56 +0400

    Update Source.cpp
    Добавил цикл, чтобы пользователь работал с калькулятором без перезапуска, пока ему это требуется

--- Calculator/Calculator/Source.cpp
+++ Calculator/Calculator/Source.cpp
@@ -1,4 +1,5 @@
 // Calculator version 1.0
+// Calculator version 1.1 Пока пользователь хочет пользоваться калькулятором, он будет работать непрерывно
 #include <iostream>
 #include <cmath>
 
@@ -8,41 +9,53 @@ int main() {
 	setlocale(LC_ALL, "Rus");
 	cout << "Добро пожаловать в мой калькулятор!" << endl;
 	cout << "В первой версии поддерживаются только целые числа!" << endl;
-	int fNum, sNum;
+	int fNum, sNum, question;
 	char operation;
-	cout << "Введите первое число: ";
-	cin >> fNum;
-	cout << "Введите второе число: ";
-	cin >> sNum;
-	cout << endl << endl;
-	cout << "\t" << "###Список команд:###" << "\n"
-		<< "\t" << "Сложение: +" << "\n"
-		<< "\t" << "Вычитание: -" << "\n"
-		<< "\t" << "Умножение: *" << "\n"
-		<< "\t" << "Целочисленное деление: /" << "\n"
-		<< "\t" << "Взятие остатка: %" << "\n" << endl;
-	cout << "Введите команду: ";
-	cin >> operation;
-	cout << fNum << " " << operation << " " << sNum << " = ";
-	switch (operation) {
-	case '+':
-		cout << fNum + sNum;
-		break;
-	case '-':
-		cout << fNum - sNum;
-		break;
-	case '*':
-		cout << fNum * sNum;
-		break;
-	case '/':
-		cout << fNum / sNum;
-		break;
-	case '%':
-		cout << fNum % sNum;
-		break;
-	default:
-		cout << "Я еще не научился выполнять такие операции:(";
-		break;
+	cout << "Хотите воспользоваться калькулятором? (1 - Д/ 0 - Н): ";
+	cin >> question;
+	if (question == 1) {
+		while (question == 1) {
+			cout << "Введите первое число: ";
+			cin >> fNum;
+			cout << "Введите второе число: ";
+			cin >> sNum;
+			cout << endl << endl;
+			cout << "\t" << "###Список команд:###" << "\n"
+				<< "\t" << "Сложение: +" << "\n"
+				<< "\t" << "Вычитание: -" << "\n"
+				<< "\t" << "Умножение: *" << "\n"
+				<< "\t" << "Целочисленное деление: /" << "\n"
+				<< "\t" << "Взятие остатка: %" << "\n" << endl;
+			cout << "Введите команду: ";
+			cin >> operation;
+			cout << fNum << " " << operation << " " << sNum << " = ";
+			switch (operation) {
+			case '+':
+				cout << fNum + sNum;
+				break;
+			case '-':
+				cout << fNum - sNum;
+				break;
+			case '*':
+				cout << fNum * sNum;
+				break;
+			case '/':
+				cout << fNum / sNum;
+				break;
+			case '%':
+				cout << fNum % sNum;
+				break;
+			default:
+				cout << "Я еще не научился выполнять такие операции:(";
+				break;
+			}
+			cout << endl << "Хотите воспользоваться калькулятором? (1 - Д/ 0 - Н): ";
+			cin >> question;
+		}
+		cout << "Спасмбо за использование калькулятора! Приходите еще!";
+	}
+	else {
+		cout << "Спасибо, что зашли!";
 	}
 	return 0;
 }
