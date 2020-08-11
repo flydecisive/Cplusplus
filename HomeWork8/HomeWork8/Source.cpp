090d7629dd89927a4f9a24a10430a15d4e689776
flydecisive <45427901+flydecisive@users.noreply.github.com>
2020-08-11 22:50:30 +0400

    Update Source.cpp
    hoome work upd

--- HomeWork8/HomeWork8/Source.cpp
+++ HomeWork8/HomeWork8/Source.cpp
@@ -1,4 +1,5 @@
 // Заполнить двумерный массив случайными числами
+// UPD заполнить уникальными числами в строке
 
 #include <iostream>
 #include <ctime>
@@ -7,13 +8,27 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Rus");
+	bool already;
+	int rnd;
 	const int ROWS = 5;
 	const int COLS = 8;
 	int arr[ROWS][COLS];
 	srand(time(NULL));
 	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLS; j++) {
-			arr[i][j] = rand() % 100;
+		for (int j = 0; j < COLS; ) {
+			already = false;
+			//srand(time(NULL));
+			rnd = rand() % 100;
+			for (int k = 0; k < j; k++) {
+				if (arr[i][k] == rnd) {
+					already = true;
+					break;
+				}
+			}
+			if (!already) {
+				arr[i][j] = rnd;
+				j++;
+			}
 		}
 	}
 	for (int i = 0; i < ROWS; i++) {
