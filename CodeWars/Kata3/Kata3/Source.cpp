42abe72a5380d494c15be2bacb412920a7ec8420
flydecisive <45427901+flydecisive@users.noreply.github.com>
2020-08-12 13:51:14 +0400

    Update Source.cpp
    Kata from codewars

--- CodeWars/Kata3/Kata3/Source.cpp
+++ CodeWars/Kata3/Kata3/Source.cpp
@@ -1,1 +1,17 @@
 //The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+#include <iostream>
+
+using namespace std;
+
+int cockroach_speed(double s)
+{
+    return (int)(s * 10000 / 360);
+}
+
+int main() {
+    double speedInHours;
+    cin >> speedInHours;
+    cout << cockroach_speed(speedInHours);
+    return 0;
+}
